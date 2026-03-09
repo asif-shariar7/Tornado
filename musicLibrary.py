55a1ae103f2a7261852ac3b77793b2c067d23844
Asif Shariar <shariarasif0@gmail.com>
2026-03-09 21:35:23 +0600

    add music library module

---
+++ musicLibrary.py
@@ -0,0 +1,9 @@
+music = {
+    
+     "daylight" : "https://youtu.be/MoN9ql6Yymw?list=RDnOeJuoFs6sM",
+     "memories" : "https://youtu.be/lr3R-fUCDHA?list=RDlr3R-fUCDHA",
+     "around" : "https://youtu.be/tLNOce4Y4uc?list=RDlr3R-fUCDHA",
+     "travis" : "https://youtu.be/pxwm3sqAytE",
+     "telescope" : "https://youtu.be/v2-iKgBx8EA?list=RDpxwm3sqAytE"
+
+}
